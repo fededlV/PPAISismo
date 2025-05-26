abfb3475e3bc7729a83e2a16542d79300ea720eb
Matias K <matiask@MacBook-Air-de-Matias.local>
2025-05-26 16:36:44 -0300

    m

--- core/entities/AlcanceSismo.py
+++ core/entities/AlcanceSismo.py
@@ -3,3 +3,13 @@ from django.db import models
 class AlcanceSismo(models.Model):
     descripcion = models.TextField()
     nombre = models.CharField(max_length=255)
+
+    def getDatosAlcance(self):
+        """
+        Obtiene los datos del alcance del sismo.
+        :return: Diccionario con los datos del alcance.
+        """
+        return {
+            'descripcion': self.descripcion,
+            'nombre': self.nombre
+        }
