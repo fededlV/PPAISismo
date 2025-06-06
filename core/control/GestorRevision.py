a16ab497db33842df5390640bfe70f00edc745fa
Matias K <matiask@MacBook-Air-de-Matias-3.local>
2025-06-06 20:21:35 -0300

    Enhance GestorRevision class with session management and improve method signatures; add Sesion model for user sessions

--- core/control/GestorRevision.py
+++ core/control/GestorRevision.py
@@ -9,17 +9,19 @@ from ..entities.Usuario import Usuario
 from typing import List
 from ..entities.Estado import Estado
 from ..boundaries import PantallaRevision
+from ..entities.Sesion import Sesion
 
 
 class GestorRevision:
-    def __init__(self, eventoSismicoSeleccionado: EventoSismico = None, pantallaRevision:PantallaRevision=None):
-        self.eventosSismicosAd = []
+    def __init__(self, eventosSismicosAd: list[EventoSismico] = None, eventoSismicoSeleccionado: EventoSismico = None, pantallaRevision:PantallaRevision=None):
+        self.eventosSismicosAd = eventosSismicosAd if eventosSismicosAd is not None else []
         self.eventosSismicos = EventoSismico.objects.all()
         self.eventoSismicoSeleccionado = eventoSismicoSeleccionado
         self.pantallaRevision= pantallaRevision
         self.estados = Estado.objects.all()
         self.accionSeleccionada = None
         self.asLogueado = Usuario.objects.get()  # Usuario logueado
+        self.sesion = Sesion(usuario=Usuario.objects.get(), fecha_inicio=timezone.now(), fecha_fin=None)  # Sesión del usuario
 
     # 3 Tomar opción seleccionada
     def tomarOpcSeleccionada(self):
@@ -29,10 +31,10 @@ class GestorRevision:
         return self.eventosSismicosAd
 
     # 4 Buscar eventos sísmicos
-    def buscarEventosSismicos(self) -> None:
-        for i in self.eventosSismicos:
-            if i.obtenerEventosAd():
-                self.eventosSismicosAd.append(i)
+    def buscarEventosSismicos(self) -> list[EventoSismico]:
+        for evento in self.eventosSismicos:
+            if evento.obtenerEventosAd():
+                self.eventosSismicosAd.append(evento)
 
 
 
@@ -49,14 +51,13 @@ class GestorRevision:
 
     # 14 Tomar evento sismico
     def tomarEvento(self, evento_id: int) -> None:
-        self.eventoSismicoSeleccionado = next((evento for evento in self.eventosSismicosAd if evento.id == evento_id), None)
+        self.eventoSismicoSeleccionado = self.eventosSismicos.get(id=evento_id)
         try:
             estado_bloqueado = self.buscarEstadoBloqueado()
             if estado_bloqueado:
                 fechaYHoraActual = self.obtenerFechaHoraActual()
                 self.bloquearEvento(fechaYHoraActual, estado_bloqueado)
                 self.eventoSismicoSeleccionado.estadoActual = estado_bloqueado
-                print(f"(: Evento {evento_id} bloqueado exitosamente")
                 return self.mostrarAlcance()
                 
         except EventoSismico.DoesNotExist:
@@ -76,8 +77,7 @@ class GestorRevision:
     
     # 19 Bloquear evento
     def bloquearEvento(self, fechaHoraActual: datetime, estado: Estado) -> None:
-        eventoSismicoSeleccionado = self.eventoSismicoSeleccionado
-        eventoSismicoSeleccionado.bloquear(fechaHoraActual, estado)
+        self.eventoSismicoSeleccionado.bloquear(fechaHoraActual, estado)
           
     # 25 Mostrar alcance
     def mostrarAlcance(self) -> dict:
@@ -161,7 +161,7 @@ class GestorRevision:
 
     # 63 Obtener empleado logueado
     def obtenerEmpleadoLogueado(self): 
-        return self.asLogueado.getAsLogueado()
+        return self.sesion.getUsuarioLogueado()
     
     # 67 Buscar estados rechazados
     def buscarEstadoRechazado(self) -> List[Estado]:
