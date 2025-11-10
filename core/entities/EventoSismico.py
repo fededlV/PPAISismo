3985e72386fc83be2b302b75c67684b001d8d89c
matiask400 <mkorochresk@gmail.com>
2025-11-10 15:50:16 -0300

    space

--- core/entities/EventoSismico.py
+++ core/entities/EventoSismico.py
@@ -17,7 +17,6 @@ class EventoSismico(models.Model):
     longitudEpicentro = models.FloatField()
     longitudHipocentro = models.FloatField()
     valorMagnitud = models.FloatField()
-
     estadoActual = models.ForeignKey(Estado, on_delete=models.PROTECT, related_name='eventos_actuales')
     alcanceSismico = models.ForeignKey(AlcanceSismico,on_delete=models.PROTECT, related_name='eventos_alcance')
     clasificacion = models.ForeignKey(ClasificacionSismo,on_delete=models.PROTECT,related_name='eventos_clasificados')
